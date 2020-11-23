eac2b5e5a9c953f3095dab617f4c6d6311a29073
Amer Kalac <amerk@stud.ntnu.no>
2020-11-23 21:14:51 +0100

    Clean up structure and processing scripts

--- Data/Clean and processed/initialpre.py
+++ Data/Clean and processed/initialpre.py
@@ -1,29 +1,33 @@
 import pandas as pd
 import numpy as np
 
-#Find average score and remove the three score columns. Also, label scores above or below average for out binary classification
-#(might be more elegant solutions to theses scripts)
+#Find mean score and remove the three score columns. Then, label scores above or below average for binary classification
 
+#Read csv, and rename columns for simplicity
 df1 = pd.read_csv('StudentsPerformance_1.csv')
 df1.rename(columns={'parental level of education': 'parent edu', 'test preparation course': 'prep course'}, inplace=True)
 print(df1.head(10))
 
+#Iterate through the data, and find mean of the three scores in the row. Append the mean to avg_score list.
+#Although one should not iterate through a dataframe, it is fine here as the dataset is small.
 df2 = df1
-
 avg_score = []
 for row in df2.itertuples():
-    score = (row._6 + row._7 + row._8) / 3   #row.COLUMNNAME. For some reason the column names were returned as _6, _7 and _8
+    score = (row._6 + row._7 + row._8) / 3   #row.COLUMNNAME is used to access the right column. For some reason, these column names were returned as _6, _7 and _8.
     score = np.round(score, 3)
     avg_score.append(score)
 
-df2['score'] = avg_score  # Add list as a column in dataframe with colname "score"
+df2['score'] = avg_score  # Add list as a column in dataframe with column name "score", which is our new score metric.
 
+#We remove the original scores from the dataframe, as we don't need them anymore.
 del df2['math score']
 del df2['reading score']
 del df2['writing score']
 
 print(df2.head(10))
 
+
+#find the mean score for the whole dataset
 def mean_value(lst):
     return sum(lst) / len(lst)
 
@@ -32,8 +36,11 @@ print('Mean score for all 1000 students is ' + str(mean_score) + '\n')
 #Or just use df2.describe. Either way, mean score is 67.771
 mean = 67.771
 
-df3 = df2
 
+#To turn all scores to binary categories:
+#Iterate through the rows in the dataframe. If the score is above or equal to mean, "above avg" is appended to our new list.
+#Opposite if the score is below the mean value found above.
+df3 = df2
 scoreclass = []
 for row in df3.itertuples():
     if (row.score >= mean):
@@ -41,12 +48,14 @@ for row in df3.itertuples():
     else:
         scoreclass.append('below avg')
 
-df3['scoreclf'] = scoreclass  # Add list as a column in dataframe
+df3['scoreclf'] = scoreclass  #The list is appended to our dataframe
 print(df3.head(10))
 
-df3 = df3.drop('score', axis=1)
+df3 = df3.drop('score', axis=1) #We drop the numeric score variable, as we no longer need it
+#df.head() is used throughout the code to check how the dataframe looks. All numeric scores should now
+#be replaced with one column stating whether the combined score (total student performance) is below or average.
 print(df3.head(10))
 
 
-#Create a new CSV file for encoding scripts
-#df3.to_csv('Initial.csv', na_rep='UNKNOWN', index=False)
+#Create a new CSV file called "Initial.csv" for further processing (the encoding scripts)
+df3.to_csv('Initial.csv', na_rep='UNKNOWN', index=False)
--- Data/Clean and processed/onehotord.py
+++ Data/Clean and processed/onehotord.py
@@ -1,50 +1,45 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.preprocessing import OrdinalEncoder, LabelEncoder
-
+#Read the csv "Initial.csv" created by running the script "initialpre.py"
 df1 = pd.read_csv('Initial.csv')
 print(df1.head())
 
-#One hot encode all except parent edu:
+#One hot encode all feature variables except parent edu:
 enc_race = pd.get_dummies(df1['race/ethnicity'], prefix = 'race')
 enc_gend = pd.get_dummies(df1['gender'], prefix = 'gender')
 enc_lunch = pd.get_dummies(df1['lunch'], prefix = 'lunch')
 enc_course = pd.get_dummies(df1['prep course'], prefix = 'course')
 
+#Concatenate the dummy variables to df1, creating a new dataframe df2
 df2 = pd.concat((df1, enc_race, enc_gend, enc_lunch, enc_course), axis=1)
 
+#Drop the original columns from the raw data
 df2 = df2.drop(['gender', 'race/ethnicity', 'lunch', 'prep course'], axis=1)
 
-#drop the extra variables added due to dummy creation, for the binary variables, and rename
-df2 = df2.drop(['gender_male', 'lunch_free/reduced', 'course_none'], axis=1)
-df2.rename(columns={'gender_female': 'gender', 'lunch_standard': 'standard lunch', 'course_completed': 'completed course'}, inplace=True)
-df2.rename(columns={'race_group A': 'race A', 'race_group B': 'race B', 'race_group C': 'race C', 'race_group D': 'race D', 'race_group E': 'race E'}, inplace=True)
+#drop the extra variables added due to dummy creation, for the binary variables
+#Note that dummy creation makes one binary column for each category. We can remove one of the new
+#columns, as it is redundant. E.g. since we remove race E, we know that if all other races are 0
+#then it must mean that race E is 1, even though there is no own column for it.
+df2 = df2.drop(['gender_male', 'lunch_free/reduced', 'course_none', 'race_group E'], axis=1)
 
-#do the same for target variable
+#Rename some for simplicity
+df2.rename(columns={'gender_female': 'gender', 'lunch_standard': 'standard lunch', 'course_completed': 'completed course'}, inplace=True)
+df2.rename(columns={'race_group A': 'race A', 'race_group B': 'race B', 'race_group C': 'race C', 'race_group D': 'race D'}, inplace=True)
+
+#do the same procedure for target variable, one-hot encoding:
 enc_target = pd.get_dummies(df2['scoreclf'], prefix = 'course')
 df2 = pd.concat((df2, enc_target), axis=1)
 df2 = df2.drop(['scoreclf', 'course_below avg'], axis=1)
 df2.rename(columns={'course_above avg': 'above avg score'}, inplace=True)
 
-#Ordinl encode parent edu:
-#enc = OrdinalEncoder()
-#enc.fit(df2[['parent edu']])
-#df2[['parent edu']] = enc.transform(df2[['parent edu']])
-
-#Ordinal encode using pandas mapping:
+#Ordinal encode parental eduction using Pandas mapping. Could also have used sklearn's labelencoder or ordinalencoder.
 mapping = {"some high school": 0, "high school": 1, "some college": 2, "associate's degree": 3, "bachelor's degree": 4, "master's degree": 5}
 df2['parent edu'] = df2['parent edu'].map(lambda x: mapping[x])
 
+#The data should be processed, where everything is one-hot encoded except parental education, which is ordinal encoded:
+#df.head was used throughout the code simply to check if everything was executed as expected.
 print(df2.head(20))
 
-#Write to CSV V1:
-#df2.to_csv('OnehotOrd_1.csv', na_rep='UNKNOWN', index=False)
-
-
-#Remove the redundant race column (race E)
-df3 = df2.drop('race E', axis=1)
-print(df3.head())
-
-#And write to CSV V2:
-#df3.to_csv('OnehotOrd_2.csv', na_rep='UNKNOWN', index=False)
+#And write to a new csv file, called "OnehotOrd.csv", for use in our model implementation:
+df2.to_csv('OnehotOrd.csv', na_rep='UNKNOWN', index=False)
